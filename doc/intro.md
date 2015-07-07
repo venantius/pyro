4c29dc0554157495fe3db30e4701b9fd0dfe8a62
Ursa Americanus Kermodei <venantius@gmail.com>
2015-07-07 12:15:10 -0700

    [project] Initial project outline

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to pyro
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/pyro/core_test.clj
@@ -0,0 +1,7 @@
+(ns pyro.core-test
+  (:require [clojure.test :refer :all]
+            [pyro.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
