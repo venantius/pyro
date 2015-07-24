d9dfb564430af3561ad5ff44eb3b0b9caf6251ea
Ursa Americanus Kermodei <venantius@gmail.com>
2015-07-24 13:30:14 -0400

    [source] Source code reader in place
    This commit adds a source code reader with whitespace indentation and an
    arrow pointing to a line in question. This is intended for use in
    interactive debugging as a way of quickly showing a developer the
    suspicious line in question.

--- src/pyro/source.clj
+++ src/pyro/source.clj
@@ -1,31 +1,15 @@
 (ns pyro.source
   "A namespace for reading source code off of the classpath"
-  (:import [java.io LineNumberReader InputStreamReader PushbackReader]
+  (:import [java.io BufferedReader InputStreamReader]
            [clojure.lang RT]))
 
-#_(defn source-fn
-    "Returns a string of the source code for the given symbol, if it can
-  find it.  This requires that the symbol resolve to a Var defined in
-  a namespace for which the .clj is in the classpath.  Returns nil if
-  it can't find the source.  For most REPL usage, 'source' is more
-  convenient.
-  Example: (source-fn 'filter)"
-    [x]
-    (when-let [v (resolve x)]
-      (when-let [filepath (:file (meta v))]
-        (when-let [strm (.getResourceAsStream (RT/baseLoader) filepath)]
-          (with-open [rdr (LineNumberReader. (InputStreamReader. strm))]
-            (dotimes [_ (dec (:line (meta v)))] (.readLine rdr))
-            (let [text (StringBuilder.)
-                  pbr (proxy [PushbackReader] [rdr]
-                        (read [] (let [i (proxy-super read)]
-                                   (.append text (char i))
-                                   i)))
-                  read-opts (if (.endsWith ^String filepath "cljc") {:read-cond :allow} {})]
-              (if (= :unknown *read-eval*)
-                (throw (IllegalStateException. "Unable to read source while *read-eval* is :unknown."))
-                (read read-opts (PushbackReader. pbr)))
-              (str text)))))))
+(defn- pad
+  [s]
+  (str "    " s))
+
+(defn- pad-arrow
+  [s]
+  (str "--> " s))
 
 (defn source-fn
   "A function for pulling in source code.
@@ -35,19 +19,12 @@
   n preceding and following lines."
   [filepath line number]
   (when-let [strm (.getResourceAsStream (RT/baseLoader) filepath)]
-    (with-open [rdr (LineNumberReader. (InputStreamReader. strm))]
-      (println (- line number))
-      (.setLineNumber rdr (- line number))
-      (println (.getLineNumber rdr))
-      (println (.readLine rdr))
-      (dotimes [_ (inc (* number 2))] (.readLine rdr))
-      (let [text (StringBuilder.)
-            pbr (proxy [PushbackReader] [rdr]
-                  (read [] (let [i (proxy-super read)]
-                             (.append text (char i))
-                             i)))
-            read-opts (if (.endsWith ^String filepath "cljc") {:read-cont :allow} {})]
-        (if (= :unknown *read-eval*)
-          (throw (IllegalStateException. "Unable to read source while *read-eval* is :unknown."))
-          (read read-opts (PushbackReader. pbr)))
-        (str text)))))
+    (with-open [rdr (BufferedReader. (InputStreamReader. strm))]
+      (let [rdr (drop (- line (inc number)) (line-seq rdr))]
+        (let [pre (take number rdr)
+              line (nth rdr number)
+              post (drop (inc number) (take (inc (* number 2)) rdr))]
+          (clojure.string/join "\n" (flatten
+                                     [(map pad pre)
+                                      (pad-arrow line)
+                                      (map pad post)])))))))
--- test/pyro/source_test.clj
+++ test/pyro/source_test.clj
@@ -5,4 +5,4 @@
 (deftest source-fn-works
   (let [fp (:file (meta #'clojure.core/contains?))]
     (is (= (source/source-fn fp 1421 3)
-           ""))))
+           "      it will not perform a linear search for a value.  See also 'some'.\"\n      {:added \"1.0\"\n       :static true}\n-->   [coll key] (. clojure.lang.RT (contains coll key)))\n    \n    (defn get\n      \"Returns the value mapped to key, not-found or nil if key not present.\""))))
