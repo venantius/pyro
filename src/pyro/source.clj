a287e3b245651a94e104292c21077b2f094cfd6c
Ivar Refsdal <refsdal.ivar@gmail.com>
2018-02-19 21:35:48 +0100

    Fix evict cache on file change #12 and load-file issue #11

--- src/pyro/source.clj
+++ src/pyro/source.clj
@@ -5,9 +5,11 @@
             [glow.colorschemes :as colorschemes]
             [glow.terminal :as terminal]
             [glow.parse :as parse]
-            [instaparse.core :as insta])
+            [clojure.java.io :as io]
+            [clojure.string :as str])
   (:import [clojure.lang RT]
-           [java.io BufferedReader InputStreamReader]))
+           [java.io BufferedReader InputStreamReader]
+           (java.nio.charset StandardCharsets)))
 
 (defn pad-integer
   "Right-pad an integer with spaces until it takes up 4 character spaces."
@@ -23,13 +25,41 @@
   [s n]
   (str "--> " (pad-integer n) " " s))
 
+(defn filepath->lastModified
+  [filepath]
+  (let [url (io/resource filepath)
+        file (io/file filepath)]
+    (cond
+      ; handle files inside JARs
+      ; (io/resource "clojure/java/io.clj") =>
+      ; URL "jar:file:/Users/ivref/.m2/repository/org/clojure/clojure/1.8.0/clojure-1.8.0.jar!/clojure/java/io.clj"
+      (and url (= "jar" (.getProtocol url))) (-> url
+                                                 (.getFile)
+                                                 (str/split #"!")
+                                                 (first)
+                                                 (subs 5) ; remove file:
+                                                 (io/file)
+                                                 (.lastModified))
+
+      ; handle local files loaded using require and similar
+      ; (io/resource "pyro/source.clj") =>
+      ; URL "file:/Users/ivref/clojure/pyro/src/pyro/source.clj"
+      (and url (= "file" (.getProtocol url))) (-> url (.getFile) (io/file) (.lastModified))
+
+      ; handle files loaded using Cursive, load-file and similar
+      (.exists file) (.lastModified file)
+
+      :else (throw (ex-info "Could not find lastModified of filepath" {:filepath filepath})))))
+
 (defn filepath->buffered-reader
   [filepath]
-  (when-let [strm (.getResourceAsStream (RT/baseLoader) filepath)]
-    (BufferedReader. (InputStreamReader. strm))))
+  (if-let [strm (or (.getResourceAsStream (RT/baseLoader) filepath)
+                    (io/input-stream (io/file filepath)))]
+    (BufferedReader. (InputStreamReader. strm StandardCharsets/UTF_8))
+    (throw (ex-info "Could not get stream" {:filepath filepath}))))
 
 (defn file-source
-  [filepath]
+  [[filepath _]]
   (terminal/ansi-colorize
    colorschemes/terminal-default
    (parse/parse
@@ -46,7 +76,7 @@
   n preceding and following lines."
   {:added "0.1.0"}
   [filepath line number]
-  (let [rdr (memoized-file-source filepath)]
+  (let [rdr (memoized-file-source [filepath (filepath->lastModified filepath)])]
     (let [content (drop (- line (inc number))
                         (string/split rdr #"\n"))
           pre (take number content)
