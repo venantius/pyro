fbba2c412b727386b867abd55933a9bfb79b4a25
Ivar Refsdal <ivar.refsdal@nsd.no>
2018-02-21 10:36:53 +0100

    Document and refactor

--- src/pyro/source.clj
+++ src/pyro/source.clj
@@ -30,11 +30,18 @@
   (when (and file (.exists (io/file file)))
     (io/file file)))
 
-(defn resource->file
+(defn resource->containing-file
+  "This function returns the containing file of a resource.
+
+  This can be a JAR file for a clj resource inside a dependency,
+  or a regular clj file for a clj resource used during development."
   [filepath]
   (when-let [url (io/resource filepath)]
     (let [url-str (str url)]
       (cond (str/starts-with? url-str (str "jar:file:" File/separator))
+            ; This handles clj files inside JARs, such as
+            ; (resource->file "clojure/java/io.clj")
+            ; => #object[java.io.File 0x74900942 "/home/ire/.m2/repository/org/clojure/clojure/1.8.0/clojure-1.8.0.jar"]
             (-> url-str
                 (subs (count "jar:file:"))
                 (URLDecoder/decode "UTF-8")
@@ -43,6 +50,9 @@
                 ((fn [x] (str/join "!" x)))
                 (file-exists-or-nil))
 
+            ; This handles standalone clj files present during development, such as
+            ; (io/resource "pyro/source.clj")
+            ; => #object[java.net.URL 0x2ab4619e "file:/home/ire/code/github/pyro/src/pyro/source.clj"]
             (str/starts-with? url-str (str "file:" File/separator))
             (-> url-str
                 (subs (count "file:"))
@@ -51,7 +61,7 @@
 
 (defn filepath->lastModified
   [filepath]
-  (when-let [file (or (resource->file filepath)
+  (when-let [file (or (resource->containing-file filepath)
                       (file-exists-or-nil filepath))]
     (.lastModified file)))
 
@@ -62,14 +72,24 @@
     (BufferedReader. (InputStreamReader. strm StandardCharsets/UTF_8))))
 
 (defn file-source
-  [[filepath _]]
+  "A function for getting colorized source of filepath."
+  [filepath]
   (terminal/ansi-colorize
    colorschemes/terminal-default
    (parse/parse
     (string/join "\n" (line-seq (filepath->buffered-reader filepath))))))
 
 (def memoized-file-source
-  (lu file-source :lu/threshold 64))
+  "Returns a memoized file-source function that detects changes to files.
+
+  This function invokes an inner function taking two arguments,
+  both the filepath and the lastModified of the file.
+  This is the function that is actually memoized, and thus
+  the cache will never return old or expired files."
+  (let [invoke-file-source (fn [filepath _] (file-source filepath))
+        memoized-file-source (lu invoke-file-source :lu/threshold 64)]
+    (fn [filepath]
+      (memoized-file-source filepath (filepath->lastModified filepath)))))
 
 (defn source-fn
   "A function for pulling in source code.
@@ -79,7 +99,7 @@
   n preceding and following lines."
   {:added "0.1.0"}
   [filepath line number]
-  (let [rdr (memoized-file-source [filepath (filepath->lastModified filepath)])]
+  (let [rdr (memoized-file-source filepath)]
     (let [content (drop (- line (inc number))
                         (string/split rdr #"\n"))
           pre (take number content)
